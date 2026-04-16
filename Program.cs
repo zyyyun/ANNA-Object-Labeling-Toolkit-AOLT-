a6a9a25164a4049f9e598fa1d226be468a0d9120
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2026-04-16 15:09:50 +0900

    feat(02-01): add global exception handlers in Program.cs (RELI-01)
    - Add Application.SetUnhandledExceptionMode(CatchException)
    - Add Application.ThreadException handler with Log.Fatal + Korean MessageBox
    - Add AppDomain.CurrentDomain.UnhandledException handler with Log.Fatal
    - Prevents app crashes from unhandled exceptions

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using ASLTv1.Forms;
 using ASLTv1.Services;
+using Serilog;
 
 namespace ASLTv1
 {
@@ -10,6 +11,25 @@ namespace ASLTv1
         {
             LogService.Initialize();
             LogService.AuditAppStart();
+
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
+            Application.ThreadException += (s, e) =>
+            {
+                Log.Fatal(e.Exception, "[FATAL] UI 스레드에서 처리되지 않은 예외 발생");
+                MessageBox.Show(
+                    $"예기치 않은 오류가 발생했습니다.\n\n{e.Exception.Message}\n\n로그 파일을 확인해주세요.",
+                    "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                if (e.ExceptionObject is Exception ex)
+                {
+                    Log.Fatal(ex, "[FATAL] 백그라운드 스레드에서 처리되지 않은 예외 발생");
+                }
+            };
+
             try
             {
                 ApplicationConfiguration.Initialize();
